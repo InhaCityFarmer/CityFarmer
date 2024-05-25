429acc9fcc4abafe4436e1e2e05a4f7f6906249d
kangho1117 <kangho1117@naver.com>
2024-05-25 16:18:13 +0900

    docs(Post.kt) : 게시글에 더 많은 정보를 담기 위해 멤버변수 writer, writerUID 추가
    게시글에 더 많은 정보를 담기 위해 멤버변수 writer, writerUID 추가

--- app/src/main/java/com/CapstoneDesign/cityfarmer/object/Post.kt
+++ app/src/main/java/com/CapstoneDesign/cityfarmer/object/Post.kt
@@ -1,6 +1,8 @@
 package com.CapstoneDesign.cityfarmer.`object`
 
 data class Post(
+    var writer: String = "",//글쓴이
+    var writerUID : String = "",//글쓴이 UID
     var title : String = "", //제목
     var body : String = "" //본문
 )
