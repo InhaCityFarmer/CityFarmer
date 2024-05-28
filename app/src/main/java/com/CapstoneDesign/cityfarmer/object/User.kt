25b7e79404bd7e1f992c0a315c4fcd9e19fbcf2d
Yunji Kim <y00ndlek@gmail.com>
2024-05-28 20:16:10 +0900

    fix: spinner item 추가

--- app/src/main/java/com/CapstoneDesign/cityfarmer/object/User.kt
+++ app/src/main/java/com/CapstoneDesign/cityfarmer/object/User.kt
@@ -7,7 +7,6 @@ data class User(
     var farmName : String = "", // 농장 이름
     var address : String = "", //농장 주소
     var sector : String = "", //농장 농지 번호
-    var crop : ArrayList<HashMap<String,Array<Double>>> = ArrayList<HashMap<String,Array<Double>>>(), //유저가 키우는 작물 센서값 Array<Crop>과 동일함
     var inventory : ArrayList<Item> = ArrayList<Item>(),// 인벤토리
     var prev_chat : HashMap<String,String> = HashMap<String,String>()// 채팅 목록
 )
